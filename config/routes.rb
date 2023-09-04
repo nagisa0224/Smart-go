38388aeea4b7de77e13fe7cbdb01ae358f530847
EC2 Default User <ec2-user@ip-172-31-31-207.ap-northeast-1.compute.internal>
2023-09-04 13:25:55 +0000

    break

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,43 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  
+  devise_for :admins, skip: [:registrations, :passwords], controllers:{
+    sessions: "admin/sessions"
+  }
+  
+  
+  namespace :admin do
+    get "top" => "homes#top"
+    
+    resources :shops, except: [:new, :create, :destroy]
+    
+  end
+  
+  
+  
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+  
+  
+  scope module: :public do
+    root "homes#top"
+    get "/about" => "homes#about"
+    get "/shops/my_page" => "shops#show"
+    get "shops/information/edit" => "shops#edit"
+    patch "shops/information" => "shops#update"
+    get "shops/confirm_withdraw" => "shops#confirm_withdraw"
+    patch "shops/withdraw" => "shops#withdraw"
+    
+    resources :items
+    
+    resources :reservation_details, only: [:create, :update, :destroy]
+    
+    #notification
+    
+    resources :to_gos
+
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
